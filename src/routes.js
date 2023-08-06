1f411599df5bae62f8c84ae4ca86240b73a780e6
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-08-06 18:01:32 -0300

    config rotas

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import{BrowserRouter, Routes, Route}from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filmes from './pages/Filmes';
+import Header from './components/Header';
 
 // criando o componente de rotas
 function RoutesApp(){
   return(
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/filme/:id" element={<Filmes/>}/>
+      <Route path="/filmes/:id" element={<Filmes/>}/>
     </Routes>
     </BrowserRouter>
 
